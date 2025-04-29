2e34d8b6aba29a188b13250b5f9763349fcb23bd
Viktoria Myronova <ipz212_mvl@student.ztu.edu.ua>
2025-04-29 12:33:44 +0300

    Lab1

---
+++ Symfony/my_project_directory/src/Controller/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    #[Route('/test', name: 'app_test')]
+    public function test(): Response
+    {
+        return new Response('This is a test method!');
+    }
+}
