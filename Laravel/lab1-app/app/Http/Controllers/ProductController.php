2117bad50e3369989c37b9f8d44487c4570eda25
Viktoria Myronova <ipz212_mvl@student.ztu.edu.ua>
2025-04-29 16:13:28 +0300

    Lab2 CRUD in Laravel

---
+++ Laravel/lab1-app/app/Http/Controllers/ProductController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class ProductController extends Controller
+{
+    private const PRODUCTS = [
+        1 => ['id' => 1, 'name' => 'Laptop', 'description' => 'Gaming Laptop', 'price' => 1000],
+        2 => ['id' => 2, 'name' => 'Phone', 'description' => 'Smartphone', 'price' => 500],
+        ];
+    private function getProductItemById(array $products, $id): ?array
+    {
+        return $products[$id] ?? null;
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json(['data' => self::PRODUCTS], 200);
+    }
+
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show($id): JsonResponse
+    {
+        $product = $this->getProductItemById(self::PRODUCTS, $id);
+
+        if (!$product) {
+            return response()->json(['data' => ['error' => 'Product not found']], 404);
+        }
+
+        return response()->json(['data' => $product], 200);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $data = $request->only(['name', 'description', 'price']);
+        $id = random_int(100, 999);
+
+        $newProduct = [
+            'id' => $id,
+            'name' => $data['name'],
+            'description' => $data['description'],
+            'price' => $data['price'],
+        ];
+
+        // TODO: insert to DB
+
+        return response()->json(['data' => $newProduct], 201);
+    }
+
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function update(Request $request, $id): JsonResponse
+    {
+        $product = $this->getProductItemById(self::PRODUCTS, $id);
+
+        if (!$product) {
+            return response()->json(['data' => ['error' => 'Product not found']], 404);
+        }
+
+        $data = $request->only(['name', 'description', 'price']);
+
+        $updatedProduct = [
+            'id' => $id,
+            'name' => $data['name'] ?? $product['name'],
+            'description' => $data['description'] ?? $product['description'],
+            'price' => $data['price'] ?? $product['price'],
+        ];
+
+        // TODO: update in DB
+
+        return response()->json(['data' => $updatedProduct], 200);
+    }
+
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function destroy($id): JsonResponse
+    {
+        $product = $this->getProductItemById(self::PRODUCTS, $id);
+
+        if (!$product) {
+            return response()->json(['data' => ['error' => 'Product not found']], 404);
+        }
+
+        // TODO: delete from DB
+
+        return response()->json(['message' => "Product with ID $id deleted"], 200);
+    }
+}
--- Laravel/lab1-app/routes/web.php
+++ Laravel/lab1-app/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\TestController;
+//use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -13,7 +14,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
-Route::get('/test', [TestController::class, 'test']);
+//Route::get('/test', [TestController::class, 'test']);
+
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+Route::post('products', [ProductController::class, 'store']);
+Route::put('products/{id}', [ProductController::class, 'update']);
+Route::delete('products/{id}', [ProductController::class, 'destroy']);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
